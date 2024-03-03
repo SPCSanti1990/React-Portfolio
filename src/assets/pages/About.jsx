f31c37da2193340de1f1d2ef5e2673d7415633b5
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2024-03-03 13:17:37 -0500

    added index.html, Navigation components, and moved web pages to a pages folder.

---
+++ src/assets/pages/About.jsx
@@ -0,0 +1,24 @@
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                Hi my name is Derek Santiago. I currently work for Brayton Energy as a test engineer in Hampton New Hampshire.I recieved my batchelors
+                 degree in Mechanical Engineering from the University of New Hampshire in May 2023. I also have two assocciates degrees form
+                New Hampshire Technical Institute in Mechanical Engineering and Manufacturing Engineering.
+            </p>
+            <p>
+                I have been at Brayton Energy for 3 and half years and started as a mechanical technition operating heavy equipment, 
+                assembling heat exchangers and recuperators, welding and fabricating project test equipment, and assembling testing
+                high pressure gas lines. Since recieving my batchelors degree I have transitioned out of the technition role and more
+                into an engineer role woking on designing componets in SolidWorks, running tests with National Instuments LabView, seting up
+                required DAQ equipent for testing, and working with high and low voltage lines for operating test equiment.
+            </p>
+            <p>
+                 I am an Army veteran who was stationed in Fort Drum New York for 4 years and currently still serve in the Army Reserves.
+                Combined I have 12 years in service with one deployment to Syria. My MOS is a 91B also know as diesel technition. While in 
+                the reserves I have been certified in crane lifing opperations and vehical recovery opperations.
+            </p>
+        </div>
+    )
+}
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,44 @@
+import { Link, useLocation } from 'react-router-dom';
+
+function NavTabs() {
+    const currentPage = useLocation().pathname;
+
+    return (
+        <ul className='nav nav-tabs'>
+            <li className='nav-item'>
+                <Link
+                    to="/"
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >
+                    About
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link
+                    to="/Portfolio"
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >
+                    Portfolio
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link
+                    to="/Resume"
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >
+                    Resume
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link
+                    to="/Contact"
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact
+                </Link>
+            </li>
+        </ul>
+    )
+}
+
+export default NavTabs;
